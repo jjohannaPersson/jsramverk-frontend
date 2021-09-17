4598a9b93cd4f6f288254dc8d9f21b8048263b72
jjohannaPersson <jjohanna.persson@live.se>
2021-09-17 12:05:48 +0200

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the headline JSramverk', () => {
+  const { container } = render(<App />);
+
+  expect(screen.getByText('JSramverk')).toBeInTheDocument();
+});
+
+test('If the button "Skapa nytt dokument" renders editor', () => {
+  const { container } = render(<App />);
+
+  const button = screen.getByText('Skapa nytt dokument')
+  fireEvent.click(button)
+  expect(screen.getByText('Titel')).toBeInTheDocument();
+  expect(screen.getByText('Spara')).toBeInTheDocument();
+  expect(screen.getByText('Tillbaka')).toBeInTheDocument();
+});
+
+test('If title is empty when creating a new document', () => {
+    const container = render(<App />);
+    const input = container.getByLabelText('cost-input')
+
+    expect(input.value).toBe("")
 });
